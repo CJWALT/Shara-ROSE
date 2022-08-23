f17ebb780bce5a53de458555c4861aeb9fae74b1
Emmanuel <oyedelee22@gmail.com>
2022-08-23 18:02:01 +0100

    make donation page, with media queries

--- js/main.js
+++ js/main.js
@@ -85,15 +85,3 @@ function prevSlide(){
     }
     updateSlidePosition();
 }
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    grid: {
-      rows: 2,
-    },
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
